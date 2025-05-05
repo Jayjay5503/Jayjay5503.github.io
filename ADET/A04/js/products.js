b1b96c80efa5efe0b6dfce0d7398677d4a383036
Jayjay5503 <jayjaymercadomonterola@gmail.com>
2025-05-05 19:48:29 +0800

    A04 POS

---
+++ ADET/A04/js/products.js
@@ -0,0 +1,74 @@
+var products = [
+    {
+        "category": "Takoyaki",
+        "contents": [
+            { "isAvailable": true, "name": "Baby Octopus", "price": 150, "image": "takoyaki/babyocto" },
+            { "isAvailable": true, "name": "Cheesy Bacon", "price": 120, "image": "takoyaki/cheesy" },
+            { "isAvailable": true, "name": "Shrimp and Veggies", "price": 120, "image": "takoyaki/shrimp" },
+            { "isAvailable": true, "name": "Crab and Cheese", "price": 120, "image": "takoyaki/crab" },
+            { "isAvailable": true, "name": "Octobits", "price": 120, "image": "takoyaki/octobits" },
+            { "isAvailable": true, "name": "All Flavor (24pcs)", "price": 700, "image": "takoyaki/allflavor" }
+        ]
+    },
+    {
+        "category": "Coffee",
+        "contents": [
+            { "isAvailable": true, "name": "Iced Cafe Au Lait", "price": 120, "image": "coffee/Aulait" },
+            { "isAvailable": true, "name": "Iced Cafe Americano", "price": 110, "image": "coffee/americano" },
+            { "isAvailable": true, "name": "Iced Vietnamese Coffee", "price": 120, "image": "coffee/vietnamese" },
+            { "isAvailable": true, "name": "Black Coffe (Hot)", "price": 85, "image": "coffee/black" },
+            { "isAvailable": true, "name": "Brewed Coffee", "price": 85, "image": "coffee/brewed" }
+        ]
+    },
+    {
+        "category": "Drinks",
+        "contents": [
+            { "isAvailable": true, "name": "Coke (In-can)", "price": 55, "image": "drinks/coke" },
+            { "isAvailable": true, "name": "Pineapple Juice (In-can)", "price": 55, "image": "drinks/pineapple" },
+            { "isAvailable": true, "name": "Sprite (In-can)", "price": 55, "image": "drinks/sprite" },
+            { "isAvailable": true, "name": "Iced Tea", "price": 50, "image": "drinks/icedtea" },
+            { "isAvailable": true, "name": "San Mig Apple", "price": 70, "image": "drinks/sanmig apple" },
+            { "isAvailable": true, "name": "Strong Zero", "price": 70, "image": "drinks/strongzero" },
+            { "isAvailable": true, "name": "Bottled Water", "price": 50, "image": "drinks/water" }
+        ]
+    },
+    {
+        "category": "Grilled",
+        "contents": [
+            { "isAvailable": true, "name": "Grilled Chicken Half with Rice", "price": 250, "image": "grilled/chicken-large" },
+            { "isAvailable": true, "name": "Grilled Chicken Whole", "price": 400, "image": "grilled/whole" },
+            { "isAvailable": true, "name": "Grilled Liempo", "price": 180, "image": "grilled/liempo" }
+        ]
+    },
+    {
+        "category": "Snacks",
+        "contents": [
+            { "isAvailable": true, "name": "Bacon Fries Overload", "price": 210, "image": "snack/bacon" },
+            { "isAvailable": true, "name": "Gyoza", "price": 210, "image": "snack/gyoza" },
+            { "isAvailable": true, "name": "Cheesy Beef Nachos", "price": 200, "image": "snack/nachos" },
+            { "isAvailable": true, "name": "Flavored Fries", "price": 150, "image": "snack/flavoredfries" },
+            { "isAvailable": true, "name": "Kani Tempura", "price": 210, "image": "snack/kani" },
+            { "isAvailable": true, "name": "Tempura", "price": 210, "image": "snack/tempura" },
+            { "isAvailable": true, "name": "Cheesestick", "price": 120, "image": "snack/cheesestick" }
+        ]
+    },
+    {
+        "category": "Taiwanese Chicken",
+        "contents": [
+            { "isAvailable": true, "name": "Taiwanese Chicken Salad & Iced Tea", "price": 190, "image": "TC/TCsalad" },
+            { "isAvailable": true, "name": "Taiwanese Chicken Rice & Iced Tea", "price": 215, "image": "TC/TCrice" },
+            { "isAvailable": true, "name": "Taiwanese Chicken Fries & Drinks", "price": 190, "image": "TC/TCfries" },
+            { "isAvailable": true, "name": "6pcs. Taiwanese Chicken", "price": 650, "image": "TC/TC6pcs" }
+        ]
+    },
+    {
+        "category": "Silog",
+        "contents": [
+            { "isAvailable": true, "name": "Longsilog", "price": 130, "image": "silog/longsilog" },
+            { "isAvailable": true, "name": "Baconsilog", "price": 150, "image": "silog/baconsilog" },
+            { "isAvailable": true, "name": "Samgyupsilog", "price": 150, "image": "silog/samgyup" },
+            { "isAvailable": true, "name": "Sausagesilog", "price": 120, "image": "silog/sausage" },
+            { "isAvailable": true, "name": "Shanghaisilog", "price": 120, "image": "silog/shanghai" }
+        ]
+    }
+];
